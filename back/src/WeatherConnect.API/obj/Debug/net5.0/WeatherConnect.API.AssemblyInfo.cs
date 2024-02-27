54434073c9c62f9cd7a61f552f9dd17a2901caf4
Gabriel Amorim <90906766+GabrielAm0@users.noreply.github.com>
2024-02-26 21:43:25 -0300

    Ajustes

--- back/src/WeatherConnect.API/obj/Debug/net5.0/WeatherConnect.API.AssemblyInfo.cs
+++ back/src/WeatherConnect.API/obj/Debug/net5.0/WeatherConnect.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherConnect.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6b879f01a3967a61b4faf3d422cb2d6ac10b5c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherConnect.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherConnect.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
